2e44ca48ac7e037e40b30080c8cf9a5320caa776
Alejandro Areco <arecoalejandro@gmail.com>
2020-07-21 09:07:58 -0700

    ordeno repo y comento method override en app.js

--- fogones/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const rutaLogin = require('./routes/login');
-
-
-app.listen(3030,() => console.log("Server corriendo en puerto 3030"));
-app.use('/login',rutaLogin);
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname+ '/public'));
