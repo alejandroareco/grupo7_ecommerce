293d777c3fd283f6d2d3946bf5ad6301fef4fbf6
patricio1901 <patofernandez14@gmail.com>
2020-07-24 18:59:12 -0300

    agrego controller para crear productos (no funciona)

--- fogonesPACA/controllers/cargaProductoController.js
+++ fogonesPACA/controllers/cargaProductoController.js
@@ -1,7 +1,61 @@
 const indexController = {
+
     cargaProducto:function(req, res){
         res.render('cargaProducto')
     },
+
+    listadoProductos: function(req, res){
+        res.render('home')
+    },
+
+    crearProducto:function(req, res){
+        let producto = {   
+            id: req.body.id,
+            name: req.body.name,
+            description: req.body.name,
+            image: req.body.image,
+            stock: req.body.stock,
+            price: req.body.price
+        }
+        let archivoProducto = fs.readFileSync('./data/productos.json', {encoding:'utf-8'});
+        let productos;
+        if(archivoProducto == ""){
+            productos = [];
+        }else {
+            productos = JSON.parse(archivoProducto);
+        }
+        productos.push(producto);
+        
+        productosJSON = JSON.stringify(productos);
+
+        fs.writeFileSync('./data/productos.json', productosJSON);
+
+        res.render('home');
+      
+    },
+
+/*
+    detalleDeProducto:function(req, res){
+        
+    },
+
+
+    editar:function(req, res){
+
+    },
+
+    editado:function(req, res){
+
+    },
+
+    eliminado:function(req, res){
+
+    },
+
+     productos:function(req, res){
+        res.render('home')
+    },
+    */
 }
 
 
--- fogonesPACA/routes/cargaProducto.js
+++ fogonesPACA/routes/cargaProducto.js
@@ -2,6 +2,17 @@ const express = require ('express');
 const router = express.Router();
 const cargaProductoController = require('../controllers/cargaProductoController');
 
-router.get('/',cargaProductoController.cargaProducto);
+router.get('/',cargaProductoController.cargaProducto);/*Formulario de carga de producto*/
+router.get('/', cargaProductoController.listadoProductos); /*Listado de todos los productos cargados*/
+router.post('/cargaProducto', cargaProductoController.crearProducto); /*(enviar producto agreado a archivo json)*/
+
+/*
+faltan agregar funcionalidad en cargaProductoController.js
+
+router.get('/products/:id',cargaProductoController.detalleDeProducto); Detalle de un producto determinado
+router.get('/products/:id/edit', cargaProductoController.editar); Formulario de edicion de productos
+router.put('products/:id',cargaProductoController.editado); Enviar producto editado a archivo json
+router.delete('/products/:id',cargaProductoController.eliminado); Borrar producto del json
+*/
 
 module.exports = router;
