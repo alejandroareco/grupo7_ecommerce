038fa83128352f60ce59f42b15a058d86ea38b17
ramiro-m <ramaniac@hotmail.com>
2020-08-01 14:37:50 -0300

    seguimiento de las fotos

--- fogonesPACA/controllers/cargaProductoController.js
+++ fogonesPACA/controllers/cargaProductoController.js
@@ -15,9 +15,9 @@ const indexController = {
             name: req.body.name,
             description: req.body.description,
             /* aca saca los nombres que le da multer con date.now()(tiene 3 posiciones el array req.files[2])*/
-            image: req.files[0].filename,
-            image1: req.files[1].filename,
-            image2: req.files[2].filename,
+            image: (req.files[0] == undefined) ? 'empty' : req.files[0].filename,
+            image1: (req.files[1] == undefined) ? 'empty' : req.files[1].filename,
+            image2: (req.files[2] == undefined) ? 'empty' : req.files[2].filename,
 
             stock: req.body.stock,
             price: req.body.price,
