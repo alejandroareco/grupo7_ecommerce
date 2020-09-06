6340f00e71baebf0a34b3599b41c7a6377a78933
patricio1901 <patofernandez14@gmail.com>
2020-09-06 16:26:33 -0300

    validacion front form productos

--- fogonesPACA/public/js/js-front.js
+++ fogonesPACA/public/js/js-front.js
@@ -4,39 +4,47 @@ function qs(elemento) {
 
 window.addEventListener('load', function(){
     let campoNombre = qs('input#nombre');
-    let campoPrecio = qs('div#precio');
+    let campoPrecio = qs('input#precio');
     let campoStock = qs('div#stock');
     let campoDetalles = qs('textarea#detalles');
-    let errorNombre = qs('small#errorNombre')
+    let boton = qs('button#botonMobile');
+
+    let errorNombre = qs('small#errorNombre');
+    let errorPrecio = qs('small#errorPrecio');
+    let errorDetalles = qs('small#detalles');
     
   campoNombre.addEventListener('blur', function(){
-      if(campoNombre.value.length == 0){
-        errorNombre.innerText = 'El campo no puede estar vacío!'
-      }
+      if(campoNombre.value.length < 5){
+        errorNombre.innerText = 'El campo debe tener al menos 5 caracteres!'
+      }else errorNombre.innerHTML = '';
     });
 
-  campoPrecio.addEventListener('click', function(event){
-    alert('elemento seleccionado')
+  campoPrecio.addEventListener('blur', function(event){
+    if(campoPrecio.value.length == 0){
+      errorPrecio.innerText = 'Debe indicar un precio para este producto'
+    }else errorPrecio.innerHTML = '';
   });
     
   campoStock.addEventListener('click', function(event){
-    alert('elemento seleccionado')
+    
   });
-  campoDetalles.addEventListener('click', function(event){
-    alert('elemento seleccionado')
+
+  campoDetalles.addEventListener('blur', function(event){
+    if(campoDetalles.value.length < 20){
+      qs('small#detalles').innerText = 'El campo debe tener al menos 20 caracteres!'
+    }else errorDetalles.innerHTML = '';
   });
 
 
 /*
-  form.addEventListener('submit', function (event) {
+  boton.addEventListener('submit', function (event) {
         // si el campo de correo electrónico es válido, dejamos que el formulario se envíe
-         if(!) {
+         if(true) {
           // Si no es así, mostramos un mensaje de error apropiado
-          showError();
+          showError(errorDetalles + errorPrecio + errorNombre);
           // Luego evitamos que se envíe el formulario cancelando el evento
           event.preventDefault();
         }
-      });
-      */
+      });      */
 })
 
