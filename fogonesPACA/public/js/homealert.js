4fe23372ec0afac7900fd4acaf1070e3abdad3b2
Alejandro Areco <arecoalejandro@gmail.com>
2020-09-14 15:31:34 -0700

    alerta de precaucion al inicio agregada

--- fogonesPACA/public/js/homealert.js
+++ fogonesPACA/public/js/homealert.js
@@ -1,5 +1,14 @@
-Swal.fire(
-    'ATENCION',
-    'El uso de los fogones puede causar daños. Utilizar con precaución.',
-    'warning'
-  )
+//Swal.fire(
+//    'ATENCION',
+//    'El uso de los fogones puede causar daños. Utilizar con precaución.',
+//    'warning'
+//  )
+
+Swal.fire({
+  icon: 'warning',
+  title: 'ATENCION',
+  text: 'El uso de los fogones puede causar daños. Utilizar con precaución.',
+  footer: '<a href=http://localhost:4000/productos/instructivo> Instructivo de Uso</a>',
+  html:
+  '<b>El uso de los fogones puede causar daños. Utilizar con precaución.</b>'
+})
