27890f9c65b55ba945735f00150f51580a674d06
patricio1901 <patofernandez14@gmail.com>
2020-09-21 23:24:42 -0300

    validaciones for edit de usuario

--- fogonesPACA/public/js/edituser.js
+++ fogonesPACA/public/js/edituser.js
@@ -1,24 +1,53 @@
-function qs(elemento) {
-    return document.querySelector(elemento);
+function getElementById(elemento){
+    return document.getElementById(elemento);
 }
 
 window.addEventListener('load', function(){
     
-    let campoNombres = qs('input#user');
-    let campoApellido = qs('input#apellido');
-    let campoMail = qs('input#mail');
-    let campoDireccion = qs('input#direccion');
-    let campoTelefono = qs('input#telefono');
+    let campoNombres = getElementById('user');
+    let campoApellido = getElementById('apellido');
+    let campoMail = getElementById('mail');
+    let campoDireccion = getElementById('direccion');
+    let campoTelefono = getElementById('telefono');
 
-    let errorNombre = qs('small#errorNombrew');
-    let errorPrecio = qs('');
-    let errorDetalles = qs('');
+    let errorNombre = getElementById('errorNombre');
+    let errorApellido = getElementById('errorApellido');
+    let errorMail = getElementById('errorMail')
+    let errorDireccion = getElementById('errorDireccion');
+    let errorTelefono = getElementById('errorTelefono');
     let errores = {};
-    
-  campoNombres.addEventListener('blur', function(){
-      if(campoNombres.value.length < 5){
-        errorNombre.innerText = 'El campo debe tener al menos 5 caracteres!'
-      }else errorNombre.innerHTML = '';
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    let letras = /[A-Za-z0-9]+/g;
+    let numero = /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}/;
+
+  campoNombres.addEventListener("blur", () => {
+          if (campoNombres.value.length < 2) {
+              errorNombre.innerText = 'El campo debe tener al menos 2 caracteres!';
+          }else
+              errorNombre.innerHTML = '';
+      });
+      campoApellido.addEventListener("blur", () => {
+        if (campoApellido.value.length < 2) {
+            errorApellido.innerText = 'El campo debe tener al menos 2 caracteres!';
+        }else
+            errorApellido.innerHTML = '';
     });
-     
+    campoMail.addEventListener('blur', function(){
+        if(emailRegex.test(campoMail.value)){
+            errorMail.innerText = "";
+        }else{
+            errorMail.innerHTML = 'Ingrese un mail valido'
+        }
+    })
+    campoDireccion.addEventListener('blur', function(){
+        if(letras.test(campoDireccion.value)){
+            errorDireccion.innerText = ""
+        }else errorDireccion.innerHTML = 'Recordá poner calle y número';
+    })
+    campoTelefono.addEventListener('blur', function(){
+        if(isNaN(campoTelefono.value)){
+            errorTelefono.innerText = "Ingresa un número valido"
+        }else errorTelefono.innerHTML = '';
+    })
+
 })
